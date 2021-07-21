90f614a46e20c04cb194dfe819548f791a548c31
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-21 12:53:19 +0900

    게임 초기 설정

--- main.js
+++ main.js
@@ -1,33 +1,58 @@
+//item이 추가될 field
 const field = document.querySelector('.game_field');
 const fieldRect = field.getBoundingClientRect();
-const CARROT_SIZE = 90;
-//field 밖으로 아이템이 나오지 않도록
-//CARROT_SIZE를 90으로 지정해서 빼준다
+
+//start버튼, 타이머, 카운트
 const startBtn = document.querySelector('.startBtn')
 const timerEl = document.querySelector('.timer')
 const countEl = document.querySelector('.count')
 
+//초기 상태 설정
 let start = false;
 let count = 0;
 let timer = undefined;
 
+//carrot 아이템 사이즈, 카운트 설정
+const CARROT_SIZE = 90;
+//field 밖으로 아이템이 나오지 않도록
+//CARROT_SIZE를 90으로 지정해서 빼준다
+const CARROT_COUNT = 7;
+
+
+//start 버튼이 클릭되면 게임 시작
 startBtn.addEventListener('click', () => {
-    if (start) {
+    if (start) { //초기 설정은 false
         // showStopButton();
-        console.log("false")
-    } else {
+        
+    } else { //true
         field.innerHTML = '';
+    //field를 초기화해서 아이템이 누적되지
+    //않도록 만든다
         init();
-        console.log("Start")
+        showStopBtn();
+        showTimerAndScore();
     }
     start = !start;
+    //버튼 누를때마다 상태변경
 })
+
+function showStopBtn() {
+    const stopIcon = document.querySelector('.fa-play');
+    stopIcon.classList.add('fa-stop')
+}
+
+function showTimerAndScore() {
+    timerEl.style.visibility = 'visible';
+    countEl.style.visibility = 'visible';
+}
 
 //getBoundingClientRect로 아이템을 넣어줄
 // 공간의 x,y축 확인
 function init() {
     addItems('carrot', 7, 'img/carrot.png')
     addItems('bug', 5, 'img/bug.png')
+
+    countEl.textContent = CARROT_COUNT;
 }
 
 function addItems(className, count, imgSrc ) {
@@ -59,4 +84,3 @@ function addItems(className, count, imgSrc ) {
 function randomNum(min, max) {
     return Math.random() * (max - min) + min;
 }
-init();
