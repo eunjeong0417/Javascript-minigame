c0a70960d8909b684f46c3261cc95cfe3b19e9e9
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-21 10:56:14 +0900

    element 요소들에 이벤트 더하기

--- main.js
+++ main.js
@@ -1,14 +1,33 @@
 const field = document.querySelector('.game_field');
 const fieldRect = field.getBoundingClientRect();
 const CARROT_SIZE = 90;
+//field 밖으로 아이템이 나오지 않도록
+//CARROT_SIZE를 90으로 지정해서 빼준다
+const startBtn = document.querySelector('.startBtn')
+const timerEl = document.querySelector('.timer')
+const countEl = document.querySelector('.count')
 
+let start = false;
+let count = 0;
+let timer = undefined;
+
+startBtn.addEventListener('click', () => {
+    if (start) {
+        // showStopButton();
+        console.log("false")
+    } else {
+        field.innerHTML = '';
+        init();
+        console.log("Start")
+    }
+    start = !start;
+})
 
 //getBoundingClientRect로 아이템을 넣어줄
 // 공간의 x,y축 확인
 function init() {
-    console.log(fieldRect);
     addItems('carrot', 7, 'img/carrot.png')
-    addItems( 'bug', 5, 'img/bug.png' )
+    addItems('bug', 5, 'img/bug.png')
 }
 
 function addItems(className, count, imgSrc ) {
@@ -17,11 +36,14 @@ function addItems(className, count, imgSrc ) {
     const x2 = fieldRect.width - CARROT_SIZE;
     const y2 = fieldRect.height - CARROT_SIZE;
 
+    
     for (let i = 0; i < count; i++) {
         const item = document.createElement('img');
+        
         item.setAttribute('class', className)
         item.setAttribute('src', imgSrc);
         item.style.position = 'absolute';
+
         const x = randomNum(x1, x2);
         const y = randomNum(y1, y2);
         //각각 x, y 좌표는
@@ -30,6 +52,8 @@ function addItems(className, count, imgSrc ) {
         item.style.left = `${x}px`;
         item.style.top = `${y}px`;
         field.appendChild(item);
+
+        
     }
 }
 function randomNum(min, max) {
